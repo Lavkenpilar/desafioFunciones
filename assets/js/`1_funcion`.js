b3c0df61ebc477eea6ac63506d1307b2a2f418da
Lavken <lavkenpilar@gmail.com>
2024-02-09 17:36:08 -0300

    create-index1-js1

---
+++ assets/js/`1_funcion`.js
@@ -0,0 +1,19 @@
+
+/* Transforma esta declaración de función a una función de expresión*/
+
+/*function example (a,b,c) {
+    return a + b + c
+}
+alert (example (3, 6, 9))
+}
+}*/
+
+/*escrito como expresión de función*/
+example = function (a, b, c) {
+    return a + b + c
+}
+alert (example (3, 6, 9))
+
+/* escrito como arrow functions*/
+/*example = (a, b, c) => a + b + c
+alert (example (3, 6, 9))*/
